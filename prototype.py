c4b77e8400ff9a3d2be88a60408e980f748b4c73
S. Mourre <smo@sophiemourre.fr>
2021-06-02 23:57:31 +0200

    Set URL as an attribute

--- prototype.py
+++ prototype.py
@@ -2,14 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 class Report:
+    def __init__(self, url):
+        self.url = url 
+
     def create_report(self):
         return None
 
-    def read_report(self, url):
-        html_text = requests.get(url).text
+    def read_report(self):
+        html_text = requests.get(self.url).text
         soup = BeautifulSoup(html_text, 'html.parser')
 
         return f'{soup.title}'
 
     def convert_report_to_xml(self):
         return None
+
+if __name__="__MAIN__":
+    url_test = "https://www.raidbots.com/simbot/report/w55rdJY7HwiAjqkmWJzxGU" 
+    report = Report(url_test)
--- test_prototype.py
+++ test_prototype.py
@@ -3,6 +3,6 @@ from prototype import Report
 
 def test_read_report():
     vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
-    report = Report()
-    title = report.read_report(vgm_url)
+    report = Report(vgm_url)
+    title = report.read_report()
     assert (title == '<title>VGMusic.com - Nintendo Music</title>') 
